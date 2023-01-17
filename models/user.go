b10bd32ec0d48d553f5205191fc5b198f0ee6e7b
Imanuel Ronaldo <imanuel.ronaldo@sehatq.com>
2023-01-17 22:34:41 +0700

    hide password

--- models/user.go
+++ models/user.go
@@ -4,12 +4,12 @@ import (
 	"gorm.io/gorm"
 )
 
-type User struct{
+type User struct {
 	gorm.Model
 	Username string `gorm:"size:255;not null;unique" json:"username"`
-	Password string `gorm:"size:255;not null;" json:"password"`
+	Password string `gorm:"size:255;not null;" json:"-"`
 }
 
-func (u *User)PrepareGive(){
+func (u *User) PrepareGive() {
 	u.Password = ""
 }
